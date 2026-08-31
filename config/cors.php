ee37bf2497e5772803a00af7c4120fc6007d326e
Muhamad Febri Nugroho <68019849+mfebrinugroho@users.noreply.github.com>
2026-08-31 18:18:43 +0700

    perbaikan cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://kaserva-admin.vercel.app/',
+        'https://kaserva-admin.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
